21896444e8a2033ba566c427e95a7f5069632adc
Afsana Afroze <afsanagoat@gmail.com>
2023-06-17 21:33:15 -0500

    change 22th june

---
+++ javascript/Class_1/script.js
@@ -0,0 +1,14 @@
+
+
+document.getElementById("fruit").style.color ="green";
+document.getElementById("fruit").style.border ="10px solid orange";
+document.getElementById("fruit").style.fontSize ="36px";
+document.getElementById("fruit").style.background ="blue";
+document.getElementById("fruit").style.marginTop ="150px";
+document.querySelector("a").textContent = "Shop at Amazon";
+document.querySelector("button").addEventListener("click", function(){
+    alert("How's it going?"); });
+
+document.querySelector("p").addEventListener("mouseover" ,function(){
+    document.querySelector("p").textContent ="Cheesecake bites";
+});
--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,14 @@
-document.write(result);
+
+
+var person = {
+    fname: "Scott",
+    lname: "Lang",
+    age:40,
+    isMarried:true,
+    numOfChildren:1
+};
+
+for( var dog in person) {
+    console.log(dog);
+}
+
